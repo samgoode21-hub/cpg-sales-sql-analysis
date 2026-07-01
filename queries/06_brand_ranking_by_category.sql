3c5f3d6c2f3c995b0b558801e2d00df3be92eb42
samgoode21-hub <samgoode21@gmail.com>
2026-07-01 17:17:10 -0500

    Update 06_brand_ranking_by_category.sql

--- queries/06_brand_ranking_by_category.sql
+++ queries/06_brand_ranking_by_category.sql
@@ -4,6 +4,7 @@
 -- Business Question: Rank brands within each category 
 -- based on profit
 -- SQL Concepts: SELECT, ROUND, SUM, RANK, GROUP, ORDER BY
+
 SELECT "Brand", "Product_Category", ROUND(SUM("Profit_USD")::numeric,2) AS Profit,
 RANK() OVER(PARTITION BY "Product_Category" ORDER BY SUM("Profit_USD")DESC) AS brands_ranked
 FROM public.fmcg_sales
